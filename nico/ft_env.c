d3271e8cb37637b2270ad5e481945a39657ca0c2
Nicolas Pichon <nipichon@f6r4s10.clusters.42paris.fr>
2026-04-13 14:55:45 +0200

    updated ft_env

--- nico/ft_env.c
+++ nico/ft_env.c
@@ -6,8 +6,26 @@
 /*   By: nipichon <nipichon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 13:30:27 by nipichon          #+#    #+#             */
-/*   Updated: 2026/04/13 13:35:09 by nipichon         ###   ########.fr       */
+/*   Updated: 2026/04/13 14:54:56 by nipichon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	ft_env(char *name, char *value, t_all_env_var *var_dir)
+{
+	t_environment_variable	*tmp;
+
+	if (!var_dir)
+		return ;
+	tmp = var_dir->first_env_var;
+	while (tmp && tmp->next)
+	{
+		if (tmp->variable_name == name)
+		{
+			tmp->variable_value = value;
+			return ;
+		}
+		tmp = tmp->next;
+	}
+}
