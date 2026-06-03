af6a3d507a4ae692d775019fd4488c3e794ac6c7
RagnaDenoms <80968700+Fang95610@users.noreply.github.com>
2026-06-03 13:13:10 +0200

    Add files via upload
    ft_export 0.5 (check if the assignation need a ft_strdup or not)

--- ft_export.c
+++ ft_export.c
@@ -6,10 +6,28 @@
 /*   By: nipichon <nipichon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 13:30:06 by nipichon          #+#    #+#             */
-/*   Updated: 2026/04/13 13:55:54 by nipichon         ###   ########.fr       */
+/*   Updated: 2026/06/03 13:11:29 by nipichon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	ft_export(t_env *first_env, char *name, char *word)
+{
+	t_env	*finder;
 
+	if (!first_env || !name)
+		return ;
+	finder = first_env;
+	while (finder)
+	{
+		if (finder->name == name)
+		{
+			finder->value == word;
+			return ;
+		}
+		finder = finder->next;
+	}
+	finder->name = name;
+	finder->value = word;
+}
