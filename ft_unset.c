fb9a33c1385389796b22c287971321250c876919
RagnaDenoms <80968700+Fang95610@users.noreply.github.com>
2026-06-03 13:23:06 +0200

    Add files via upload
    ft_unset 0.5

--- ft_unset.c
+++ ft_unset.c
@@ -6,42 +6,35 @@
 /*   By: nipichon <nipichon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 13:30:31 by nipichon          #+#    #+#             */
-/*   Updated: 2026/04/13 14:56:32 by nipichon         ###   ########.fr       */
+/*   Updated: 2026/06/03 13:22:05 by nipichon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_unset(char var_to_unset, t_all_env_var *dir_of_vars)
+void	ft_unset(t_env *first_env, char *var_to_unset)
 {
-	t_environment_variable	*tmp;
-	t_environment_variable	*tmp_2;
+	t_env	*finder;
+	t_env	*prev;
 
-	if (!dir_of_vars)
+	if (!first_env || !var_to_unset)
 		return ;
-	tmp = dir_of_vars->first_env_var;
-	if (tmp == var_to_unset)
+	if (first_env->name == var_to_unset)
 	{
-		dir_of_vars->first_env_var = dir_of_vars->first_env_var->next;
-		free (tmp);
+		first_env = first_env->next;
 		return ;
 	}
-	while (tmp || tmp->next)
+	prev = first_env;
+	finder = first_env->next;
+	while (finder)
 	{
-		tmp_2 = tmp;
-		tmp = tmp->next;
-		if (tmp->variable_name == var_to_unset)
+		if (finder->name == var_to_unset)
 		{
-			tmp_2->next = tmp->next;
-			free (tmp);
+			prev->next = finder->next;
 			return ;
 		}
+		prev = finder;
+		finder = finder->next;
 	}
-	return (ft_nothing_to_unset());
 }
 
-void	ft_nothing_to_unset(void)
-{
-	write(1, "Error\n", 6);
-	write(1, "Environment variable does not exist\n", 37);
-}
