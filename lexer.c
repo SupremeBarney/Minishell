b5fd475c5380b49e076c0ffe4dca4d1f5a239d6f
alexfran <alexfran@f1br8s19.paris.42.school>
2026-06-08 13:03:31 +0200

    feat(parsing): lecture du heredoc et gestion des signaux
    - read_heredoc avec expansion des variables et quotes littérales
    - Ctrl-C annule le heredoc, Ctrl-C réaffiche le prompt principal
    - SIGQUIT ignoré
    - ft_calloc pour args (terminateur NULL garanti)
    - aucune fuite mémoire

--- lexer.c
+++ lexer.c
@@ -6,11 +6,126 @@
 /*   By: alexfran <alexfran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/05 13:41:29 by alexfran          #+#    #+#             */
-/*   Updated: 2026/06/02 15:37:39 by alexfran         ###   ########.fr       */
+/*   Updated: 2026/06/05 15:49:58 by alexfran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <readline/readline.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+volatile sig_atomic_t	g_signal;
+
+void	handle_sigint_heredoc(int sig)
+{
+	g_signal = sig;
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_done = 1;
+}
+
+void	handle_sigint(int sig)
+{
+	g_signal = sig;
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
+void	setup_signals(void)
+{
+	struct sigaction	sa;
+	struct sigaction	sb;
+
+	sigemptyset(&sa.sa_mask);
+	sigemptyset(&sb.sa_mask);
+	sa.sa_flags = 0;
+	sb.sa_flags = 0;
+	sa.sa_handler = handle_sigint;
+	sb.sa_handler = SIG_IGN;
+	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGQUIT, &sb, NULL);
+}
+
+void	setup_heredoc_signal(void)
+{
+	struct sigaction	sa;
+
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sa.sa_handler = handle_sigint_heredoc;
+	sigaction(SIGINT, &sa, NULL);
+}
+
+char    *heredoc_expansion(char *str, t_shell shell)
+{
+	int		i;
+	int		flag;
+	char	*res;
+	char	*tmp_dollar;
+	char	tmp[2];
+
+	i = ((flag = 0));
+	res = ft_strdup("");
+	tmp[1] = 0;
+	while (str[i])
+	{
+		if (str[i] == '$')
+		{
+			tmp_dollar = handle_dollar(str, &i, shell);
+			res = strjoin_free(res, tmp_dollar);
+			free(tmp_dollar);
+		}
+		else
+		{
+			tmp[0] = str[i++];
+			res = strjoin_free(res, tmp);
+		}
+	}
+	return (res);
+}
+
+char	*strjoin_free(char *s1, char *s2)
+{
+	char	*res;
+
+	res = ft_strjoin(s1, s2);
+	free (s1);
+	return (res);
+}
+
+char	*read_heredoc(char *delimiter, t_shell shell)
+{
+	char	*res;
+	char	*tmp;
+	char	*expanse;
+
+	res = ft_strdup("");
+	while (1)
+	{
+		tmp = readline("> ");
+		if (!tmp)
+			break ;
+		if (g_signal == SIGINT)
+			return (free(res), free(tmp), NULL);
+		expanse = heredoc_expansion(tmp, shell);
+		free (tmp);
+		tmp = expanse;
+		if (!ft_strncmp(delimiter, tmp, ft_strlen(delimiter) + 1))
+		{
+			free (tmp);
+			break ;
+		}
+		res = strjoin_free(res, tmp);
+		res = strjoin_free(res, "\n");
+		free (tmp);
+	}
+	return (res);
+}
 
 int	nb_of_tokens(t_token *token)
 {
@@ -38,7 +153,7 @@ void	init_cmd(t_cmd *cmd)
 	cmd->next = NULL;
 }
 
-void	add_cmd_node(t_cmd **cmd, t_token *tokens)
+int	add_cmd_node(t_cmd **cmd, t_token *tokens, t_shell shell)
 {
 	int		i;
 	t_token	*cur_token;
@@ -48,12 +163,12 @@ void	add_cmd_node(t_cmd **cmd, t_token *tokens)
 	cur_token = tokens;
 	*cmd = ft_calloc(1, sizeof(t_cmd));
 	if (!(*cmd))
-		return ;
+		return (0);
 	init_cmd(*cmd);
 	cur_cmd = *cmd;
-	cur_cmd->args = malloc(sizeof(char *) * (nb_of_tokens(tokens) + 1));
+	cur_cmd->args = ft_calloc(nb_of_tokens(cur_token) + 1, sizeof(char *));
 	if (!cur_cmd->args)
-		return ;
+		return (0);
 	while (cur_token)
 	{
 		if (cur_token->token_type == WORD)
@@ -65,12 +180,11 @@ void	add_cmd_node(t_cmd **cmd, t_token *tokens)
 			cur_token = cur_token->next;
 			cur_cmd->next = ft_calloc(1, sizeof(t_cmd));
 			if (!cur_cmd->next)
-				return ;
+				return (0);
 			cur_cmd = cur_cmd->next;
-			cur_cmd->args = malloc(sizeof(char *)
-					* (nb_of_tokens(cur_token) + 1));
+			cur_cmd->args = ft_calloc(nb_of_tokens(cur_token) + 1, sizeof(char *));
 			if (!cur_cmd->args)
-				return ;
+				return (0);
 			continue ;
 		}
 		else if (cur_token->token_type == REDIR_IN)
@@ -80,8 +194,16 @@ void	add_cmd_node(t_cmd **cmd, t_token *tokens)
 		}
 		else if (cur_token->token_type == HEREDOC)
 		{
+			setup_heredoc_signal();
 			cur_token = cur_token->next;
-			cur_cmd->heredoc = ft_strdup(cur_token->token);
+			cur_cmd->heredoc = read_heredoc(cur_token->token, shell);
+			setup_signals();
+			if (!cur_cmd->heredoc)
+			{
+				free_cmd(*cmd);
+				*cmd = NULL;
+				return (1);
+			}
 		}
 		else if (cur_token->token_type == REDIR_OUT)
 		{
@@ -96,6 +218,7 @@ void	add_cmd_node(t_cmd **cmd, t_token *tokens)
 		cur_token = cur_token->next;
 	}
 	cur_cmd->args[i] = NULL;
+	return (0);
 }
 
 int	nb_of_quotes(char *token)
@@ -118,7 +241,7 @@ int	nb_of_quotes(char *token)
 			count_quote++;
 		}
 		i++;
-}
+	}
 	return (count_quote);
 }
 
@@ -170,124 +293,6 @@ t_token_type	which_type(char *token)
 	}
 	else
 		return (WORD);
-}
-
-void	add_node(t_token **tokens, char *token)
-{
-	t_token	*cur;
-	t_token	*new_token;
-
-	if (!token)
-		return ;
-	new_token = malloc(sizeof(t_token));
-	if (!new_token)
-		return ;
-	new_token->token = no_quotes(token);
-	new_token->token_type = which_type(token);
-	new_token->next = NULL;
-	free(token);
-	if (!*tokens)
-		*tokens = new_token;
-	else
-	{
-		cur = *tokens;
-		while (cur->next)
-			cur = cur->next;
-		cur->next = new_token;
-	}
-}
-
-void	handle_operator(char *str, t_token **tokens, int *i, int *start_token)
-{
-	char	*tmp;
-
-	if (*i != *start_token)
-	{
-		tmp = ft_substr(str, (unsigned int)*start_token, i - start_token);
-		if (tmp)
-			add_node(tokens, tmp);
-	}
-	if (str[*i] == '|')
-	{
-		add_node(tokens, ft_strdup("|"));
-		(*i)++;
-		*start_token = *i;
-	}
-	else if (str[*i] == '<')
-	{
-		if (str[*i + 1] == '<')
-		{
-			add_node(tokens, ft_strdup("<<"));
-			*i += 2;
-			*start_token = *i;
-		}
-		else
-		{
-			add_node(tokens, ft_strdup("<"));
-			(*i)++;
-			*start_token = *i;
-		}
-	}
-	else if (str[*i] == '>')
-	{
-		if (str[*i + 1] == '>')
-		{
-			add_node(tokens, ft_strdup(">>"));
-			*i += 2;
-			*start_token = *i;
-		}
-		else
-		{
-			add_node(tokens, ft_strdup(">"));
-			(*i)++;
-			*start_token = *i;
-		}
-	}
-}
-
-void	tokenisation(char *str, t_token **tokens)
-{
-	int		i;
-	int		token_start;
-	int		quote_flag;
-	char	*tmp;
-
-	i = ((quote_flag = 0));
-	while (str[i] == ' ')
-		i++;
-	token_start = i;
-	while (str[i])
-	{
-		if (str[i] == '\'' && quote_flag != 2)
-			quote_flag = 1 - quote_flag;
-		else if (str[i] == '"' && quote_flag != 1)
-			quote_flag = 2 - quote_flag;
-		else if (str[i] == ' ' && quote_flag == 0)
-		{
-			tmp = ft_substr(str, token_start, i - token_start);
-			if (tmp)
-				add_node(tokens, tmp);
-			while (str[i] == ' ')
-				i++;
-			token_start = i;
-			continue ;
-		}
-		else if (str[i] == '|' || str[i] == '<' || str[i] == '>')
-		{
-			handle_operator(str, tokens, &i, &token_start);
-			while (str[i] == ' ')
-				i++;
-			token_start = i;
-			continue ;
-		}
-		i++;
-	}
-	if (i > token_start)
-	{
-		tmp = ft_substr(str, token_start, i - token_start);
-		if (tmp)
-			add_node(tokens, tmp);
-	}
 }
 
 void	display_node(t_token *list)
@@ -404,7 +409,6 @@ int	valid_syntax(t_token *tokens)
 	return (0);
 }
 
-
 char	**first_equal(char *str)
 {
 	int		i;
@@ -476,10 +480,12 @@ void	display_env(t_env *var)
 {
 	t_env	*cur;
 
+	if (!var)
+		return ;
 	cur = var;
 	while (cur)
 	{
-		printf("%s : %s\n", cur->name, cur->value);
+		printf("%s=%s\n", cur->name, cur->value);
 		cur = cur->next;
 	}
 }
@@ -498,15 +504,7 @@ char	*get_env_value(t_env *env, char *name)
 	return (NULL);
 }
 
-char	*strjoin_free(char *s1, char *s2)
-{
-	char	*res;
-
-	res = ft_strjoin(s1, s2);
-	free (s1);
-	return (res);
-}
-char	*handle_dollar(char *str, int *i, t_shell *shell)
+char	*handle_dollar(char *str, int *i, t_shell shell)
 {
 	int		j;
 	char	*var;
@@ -514,17 +512,24 @@ char	*handle_dollar(char *str, int *i, t_shell *shell)
 
 	(*i)++;
 	j = *i;
+	if (str[*i] == '?')
+	{
+		(*i)++;
+		return (ft_itoa(shell.exit_status));
+	}
+	else if (!ft_isalpha(str[*i]) && str[*i] != '?' && str[*i] != '_')
+		return (ft_strdup("$"));
 	while (ft_isalnum(str[*i]) || str[*i] == '_')
 		(*i)++;
 	var = ft_substr(str, j, *i - j);
-	value = get_env_value(shell->env, var);
+	value = get_env_value(shell.env, var);
 	free(var);
 	if (value)
 		return (ft_strdup(value));
 	return (ft_strdup(""));
 }
 
-char    *expansion(char *str, t_shell *shell)
+char    *expansion(char *str, t_shell shell)
 {
 	int		i;
 	int		flag;
@@ -562,6 +567,144 @@ char    *expansion(char *str, t_shell *shell)
 	return (res);
 }
 
+void	add_node(t_token **tokens, char *token, t_shell shell)
+{
+	t_token	*cur;
+	t_token	*new_token;
+
+	if (!token)
+		return ;
+	new_token = malloc(sizeof(t_token));
+	if (!new_token)
+		return ;
+	new_token->token_type = which_type(token);
+	if (new_token->token_type == WORD)
+		new_token->token = expansion(token, shell);
+	else
+		new_token->token = ft_strdup(token);
+	new_token->next = NULL;
+	free(token);
+	if (!*tokens)
+		*tokens = new_token;
+	else
+	{
+		cur = *tokens;
+		while (cur->next)
+			cur = cur->next;
+		cur->next = new_token;
+	}
+}
+
+void	handle_operator(char *str, t_token **tokens, int *i, int *start_token, t_shell shell)
+{
+	char	*tmp;
+
+	if (*i != *start_token)
+	{
+		tmp = ft_substr(str, (unsigned int)*start_token, i - start_token);
+		if (tmp)
+			add_node(tokens, tmp, shell);
+	}
+	if (str[*i] == '|')
+	{
+		add_node(tokens, ft_strdup("|"), shell);
+		(*i)++;
+		*start_token = *i;
+	}
+	else if (str[*i] == '<')
+	{
+		if (str[*i + 1] == '<')
+		{
+			add_node(tokens, ft_strdup("<<"), shell);
+			*i += 2;
+			*start_token = *i;
+		}
+		else
+		{
+			add_node(tokens, ft_strdup("<"), shell);
+			(*i)++;
+			*start_token = *i;
+		}
+	}
+	else if (str[*i] == '>')
+	{
+		if (str[*i + 1] == '>')
+		{
+			add_node(tokens, ft_strdup(">>"), shell);
+			*i += 2;
+			*start_token = *i;
+		}
+		else
+		{
+			add_node(tokens, ft_strdup(">"), shell);
+			(*i)++;
+			*start_token = *i;
+		}
+	}
+}
+
+void	tokenisation(char *str, t_token **tokens, t_shell shell)
+{
+	int		i;
+	int		token_start;
+	int		quote_flag;
+	char	*tmp;
+
+	i = ((quote_flag = 0));
+	while (str[i] == ' ')
+		i++;
+	token_start = i;
+	while (str[i])
+	{
+		if (str[i] == '\'' && quote_flag != 2)
+			quote_flag = 1 - quote_flag;
+		else if (str[i] == '"' && quote_flag != 1)
+			quote_flag = 2 - quote_flag;
+		else if (str[i] == ' ' && quote_flag == 0)
+		{
+			tmp = ft_substr(str, token_start, i - token_start);
+			if (tmp)
+				add_node(tokens, tmp, shell);
+			while (str[i] == ' ')
+				i++;
+			token_start = i;
+			continue ;
+		}
+		else if (str[i] == '|' || str[i] == '<' || str[i] == '>')
+		{
+			handle_operator(str, tokens, &i, &token_start, shell);
+			while (str[i] == ' ')
+				i++;
+			token_start = i;
+			continue ;
+		}
+		i++;
+	}
+	if (i > token_start)
+	{
+		tmp = ft_substr(str, token_start, i - token_start);
+		if (tmp)
+			add_node(tokens, tmp, shell);
+	}
+}
+
+void	free_env(t_shell *shell)
+{
+	t_env	*tmp;
+
+	if (!shell->env)
+		return ;
+	while (shell->env)
+	{
+		tmp = shell->env->next;
+		free (shell->env->name);
+		free (shell->env->value);
+		free (shell->env);
+		shell->env = tmp;
+	}
+	shell->env = NULL;
+}
+
 void	main_loop(char **envp)
 {
 	t_shell	shell;
@@ -571,22 +714,31 @@ void	main_loop(char **envp)
 
 	shell.env = NULL;
 	set_env(&shell.env, envp);
-	shell.exit_status = 0;
+	shell.exit_status = 42;
+	setup_signals();
 	while (1)
 	{
+		g_signal = 0;
 		tokens = NULL;
 		cmd = NULL;
-		//display_env(var);
 		read_line = readline("Barney$ ");
-		if (!read_line || valid_nb_quote(read_line))
-			exit(EXIT_FAILURE);
+		if (!read_line)
+		{
+			free_env(&shell);
+			exit(shell.exit_status);
+		}
+		if (valid_nb_quote(read_line))
+		{
+			free (read_line);
+			continue ;
+		}
 		if (!read_line[0])
 		{
 			free (read_line);
 			continue ;
 		}
 		add_history(read_line);
-		tokenisation(read_line, &tokens);
+		tokenisation(read_line, &tokens, shell);
 		if (!tokens)
 		{
 			free (read_line);
@@ -599,9 +751,12 @@ void	main_loop(char **envp)
 			ft_putstr_fd("Wrong Syntax\n", 2);
 			continue ;
 		}
-		display_node(tokens);
-		add_cmd_node(&cmd, tokens);
-		display_cmd(cmd);
+		if (add_cmd_node(&cmd, tokens, shell) == 1)
+		{
+			free_tokens(tokens);
+			free (read_line);
+			continue ;
+		}
 		free_tokens(tokens);
 		free_cmd(cmd);
 		free (read_line);
--- minishell.h
+++ minishell.h
@@ -6,19 +6,23 @@
 /*   By: alexfran <alexfran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 11:31:14 by nipichon          #+#    #+#             */
-/*   Updated: 2026/06/02 15:13:25 by alexfran         ###   ########.fr       */
+/*   Updated: 2026/06/05 16:35:37 by alexfran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# define _DEFAULT_SOURCE
+# define _POSIX_C_SOURCE 200809L
 # include <stdlib.h>
+# include <stdio.h>
 # include "libftprintf/ft_printf.h"
 # include <sys/types.h>
 # include <sys/wait.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <signal.h>
 
 typedef struct s_cmd
 {
@@ -59,5 +63,33 @@ typedef struct s_shell
 	t_env	*env;
 	int		exit_status;
 }	t_shell;
+
+char			*strjoin_free(char *s1, char *s2);
+char			*read_heredoc(char *delimiter, t_shell shell);
+int				nb_of_tokens(t_token *token);
+void			init_cmd(t_cmd *cmd);
+int				add_cmd_node(t_cmd **cmd, t_token *tokens, t_shell shell);
+int				nb_of_quotes(char *token);
+char			*no_quotes(char *token);
+t_token_type	which_type(char *token);
+void			display_node(t_token *list);
+void			display_cmd(t_cmd *cmd);
+int				valid_nb_quote(char *str);
+void			free_cmd(t_cmd *cmd);
+void			free_tokens(t_token *tokens);
+int				valid_syntax(t_token *tokens);
+char			**first_equal(char *str);
+void			free_chars(char **tab);
+void			set_env(t_env **var, char **envp);
+void			display_env(t_env *var);
+char			*get_env_value(t_env *env, char *name);
+char			*handle_dollar(char *str, int *i, t_shell shell);
+char			*expansion(char *str, t_shell shell);
+void			add_node(t_token **tokens, char *token, t_shell shell);
+void			handle_operator(char *str, t_token **tokens, int *i,
+int *start_token, t_shell shell);
+void			tokenisation(char *str, t_token **tokens, t_shell shell);
+void			free_env(t_shell *shell);
+void			main_loop(char **envp);
 
 #endif
