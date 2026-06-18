22b78bacf2dca418c35a1cce35c5d507a97dca9f
RagnaDenoms <80968700+Fang95610@users.noreply.github.com>
2026-06-18 12:13:40 +0200

    Add files via upload
    pwd + command control

---
+++ command_control_center.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   command_control_center.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nipichon <nipichon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/13 15:02:12 by nipichon          #+#    #+#             */
+/*   Updated: 2026/06/18 12:02:21 by nipichon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	command_control(t_cmd *com_to_exec,
+							t_shell *shell)
+{
+	if (ft_strncmp(com_to_exec->args[0], "cd", 3) == 0)
+	{
+		ft_cd(com_to_exec->args[1], shell->env);
+		return ;
+	}
+	if (ft_strncmp(com_to_exec->args[0], "echo", 5) == 0)
+	{
+		if (ft_strncmp(com_to_exec->args[1], "-n", 3) == 0)
+			ft_echo_n(com_to_exec->args[2]);
+		else
+			ft_echo(com_to_exec->args[1]);
+		return ;
+	}
+	if (ft_strncmp(com_to_exec->args[0], "env", 4) == 0)
+		return (ft_env(shell->env));
+	if (ft_strncmp(com_to_exec->args[0], "exit", 5) == 0)
+		return (ft_exit(shell->exit_status));
+	if (ft_strncmp(com_to_exec->args[0], "export", 7) == 0)
+		return (ft_export(shell->env, com_to_exec->args[1], com_to_exec->args[2]));
+	if (ft_strncmp(com_to_exec->args[0], "pwd", 4) == 0)
+		return (ft_pwd(files_dir->current_file));
+	if (ft_strncmp(com_to_exec->args[0], "unset", 6) == 0)
+		return (ft_unset(com_to_exec->args[0], env_vars));
+}
--- ft_pwd.c
+++ ft_pwd.c
@@ -6,36 +6,27 @@
 /*   By: nipichon <nipichon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 13:23:01 by nipichon          #+#    #+#             */
-/*   Updated: 2026/05/26 10:32:43 by nipichon         ###   ########.fr       */
+/*   Updated: 2026/06/18 12:12:23 by nipichon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_pwd(void) //en argument, in nous faut au moin $PWD, mais je pense que pour chaque faction avoir la liste de variable d'environement serai bien
+void	ft_pwd(t_env *pwd) //en argument, in nous faut au moin $PWD, mais je pense que pour chaque faction avoir la liste de variable d'environement serai bien
 {
-	char	*pwd;
+	char	*ret;
+	int		i;
 
-	if (/*variable d'environement $PWD existe*/)
-	{
-		printf("%s", /*variable d'env $PWD *//);
+	i = 0;
+	ret = NULL;
+	if (!pwd)
 		return ;
-	}
-	pwd = malloc(BUFFER_SIZE);
-	if (!pwd) //si erreur d'alloc 
+	else
 	{
-		memory_alloc_error(); //si l'erreur d'alloc est ecrit
-		return ; //il faut que le process d'exit ne return pas 0;
+		ret = get_env_value(pwd, "PWD");
+		if (ret)
+			printf("%s", ret);
 	}
-	if (!getcwd(pwd, BUFFER_SIZE)) //si on est dans un fichier qui a ete effacer 
-	{
-		free (pwd);
-		// il faut trouver un moyen d'afficher le dossier meme si il a ete effacer, utiliser $PWD?
-		memory_alloc_error(); //l'erreur d'alloc est ecrit
-		return ; //il faut que le process d'exit ne return pas 0;
-	}
-	printf("%s\n", pwd);
-	free (pwd);
 }
 
 /*logic a faire: trouver un moyen d'utiliser $PWD au depart du lancement de programe (au lancement du programe minishell $PWD = getcwd, et ensuite update $PWD a chaque cd par le nouveau current directory), utiliser ce $PWD pour donner le pwd, et si l'utilisateur efface $PWD, utilised cwd en tant que dernier recour
