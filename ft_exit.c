bfe5f16221246b6a8e6aa89cbde1ab0a6f872e0d
RagnaDenoms <80968700+Fang95610@users.noreply.github.com>
2026-07-31 15:33:19 +0200

    Add files via upload
    exit aucun leak

--- ft_exit.c
+++ ft_exit.c
@@ -6,17 +6,29 @@
 /*   By: nipichon <nipichon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 12:26:13 by nipichon          #+#    #+#             */
-/*   Updated: 2026/07/29 19:30:56 by nipichon         ###   ########.fr       */
+/*   Updated: 2026/07/31 15:30:30 by nipichon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static	int	ft_check_num_args(char **args)
+{
+	int	i;
+
+	i = 0;
+	while (args[i])
+		i++;
+	return (i);
+}
 
 static	int	ft_is_exitable_char_check(char *str)
 {
 	int	i;
 
 	i = 0;
+	if (!str)
+		return (1);
 	if (str[i] == '+' || str[i] == '-')
 		i++;
 	while (str[i])
@@ -30,12 +42,15 @@ static	int	ft_is_exitable_char_check(char *str)
 
 void	ft_exit(int exit_status, t_token *tokens, t_cmd *cmd, t_shell shell)
 {
+	int	args_num;
+
+	args_num = ft_check_num_args(cmd->args);
 	if (ft_is_exitable_char_check(cmd->args[1]) == 0)
 	{
 		printf("bash: exit: \"%s\": numeric argument required\n", cmd->args[1]);
 		return ;
 	}
-	if (cmd->args[2])
+	if (args_num > 2)
 	{
 		printf("bash: exit: too many arguments\n");
 		return ;
@@ -43,7 +58,8 @@ void	ft_exit(int exit_status, t_token *tokens, t_cmd *cmd, t_shell shell)
 	free_tokens(tokens);
 	free_cmd(cmd);
 	free_env(&shell);
-	free(shell.read_line);
 	clear_history();
 	exit(exit_status);
 }
+
+
