2dbf2ba27e060ad8842d1b2331e5ff8025d41fbb
Alexy <alexy.francois@protomail.com>
2026-08-11 14:13:34 +0200

    exit utilise enfin l'argument numerique fourni pour calculer le code de sortie
    Avant, ft_exit recevait shell->exit_status (le $? precedent) au lieu
    d'une valeur calculee a partir de cmd->args[1] : "exit 5" quittait avec
    le code de la commande d'avant, pas 5.
    
    Ajoute ft_exit_atoll, une conversion vers long long qui detecte les
    depassements sans jamais provoquer de comportement indefini (verifie
    avant chaque multiplication au lieu d'apres, negation faite en
    arithmetique non signee pour gerer correctement le cas limite exact
    LLONG_MIN). Un argument non numerique ou en depassement fait quitter
    le shell avec le code 2 et le message "numeric argument required",
    comme le vrai bash. Le cas "trop d'arguments" reste inchange (n'a
    jamais quitte le shell, ni chez bash ni chez nous).

--- ft_exit.c
+++ ft_exit.c
@@ -40,21 +40,68 @@ static	int	ft_is_exitable_char_check(char *str)
 	return (1);
 }
 
+static long long	ft_exit_atoll(char *str, int *overflow)
+{
+	unsigned long long	res;
+	unsigned long long	limit;
+	int					neg;
+	int					i;
+
+	i = 0;
+	neg = 1;
+	res = 0;
+	*overflow = 0;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			neg = -1;
+		i++;
+	}
+	limit = 9223372036854775807ULL + (neg == -1);
+	while (str[i])
+	{
+		if (res > (limit - (str[i] - '0')) / 10)
+			*overflow = 1;
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	if (neg == -1)
+		res = -res;
+	return ((long long)res);
+}
+
+static int	ft_exit_code(char *arg, int prev_status)
+{
+	long long	code;
+	int			overflow;
+
+	if (!arg)
+		return (prev_status);
+	if (!ft_is_exitable_char_check(arg))
+	{
+		printf("bash: exit: %s: numeric argument required\n", arg);
+		return (2);
+	}
+	code = ft_exit_atoll(arg, &overflow);
+	if (overflow)
+	{
+		printf("bash: exit: %s: numeric argument required\n", arg);
+		return (2);
+	}
+	return ((unsigned char)code);
+}
+
 void	ft_exit(int exit_status, t_token *tokens, t_cmd *cmd, t_shell shell)
 {
 	int	args_num;
 
 	args_num = ft_check_num_args(cmd->args);
-	if (ft_is_exitable_char_check(cmd->args[1]) == 0)
-	{
-		printf("bash: exit: %s: numeric argument required\n", cmd->args[1]);
-		return ;
-	}
 	if (args_num > 2)
 	{
 		printf("bash: exit: too many arguments\n");
 		return ;
 	}
+	exit_status = ft_exit_code(cmd->args[1], exit_status);
 	free_tokens(tokens);
 	free_cmd(cmd);
 	free_env(&shell);
